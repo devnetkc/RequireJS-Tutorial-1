d7dfef341ed2fa968ced0034d3de2669baf71e84
Ryan Valizan <ryan@vnetkc.com>
2020-07-15 17:32:13 +0000

    :bento: new methods file

---
+++ js/customScripts/methods.js
@@ -0,0 +1,9 @@
+function submitForm() {
+    // code here
+}
+
+function loadMorePosts() {
+    // code here
+}
+
+// the question being asked is how does RequireJS know about this files dependencies
