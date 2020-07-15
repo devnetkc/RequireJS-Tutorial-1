c8f1138e7f9888dae5e1af19690b43dfa0787468
Ryan Valizan <ryan@vnetkc.com>
2020-07-15 16:17:37 +0000

    :wrench: added js dep to requrejs cfg

--- js/config.js
+++ js/config.js
@@ -1,1 +1,34 @@
 // This is example config.js
+
+// starting in Tutorial 4 we start editing this for real and setting it up as a RequireJS config file
+
+requirejs.config({
+    // baseUrl is the parent directory location of the js files to include
+    baseUrl: 'js',
+    // add the path names for the files to add
+    paths: {
+        angular: 'angular.min',
+        extCore: 'ext-core',
+        jquery: 'jquery.min',
+        // mootools standard we host the file
+        //  mootools: 'mootols.min',
+        // mootools use a cdn
+        //  mootools: ['https://cdnjs.cloudflare.com/ajax/libs/mootools/1.6.0/mootools-core.min'],
+        // mootools use a cdn but have a local failsafe copy for backup if it fails to load
+        /*
+        mootools: [
+            'https://cdnjs.cloudflare.com/ajax/libs/mootools/1.6.0/mootools-core.min',
+            'mootools.min'
+        ],
+        */
+        // mootools use a cdn but have backup cdns before defaulting to local copy
+        mootools: [
+            'https://cdnjs.cloudflare.com/ajax/libs/mootools/1.6.0/mootools-core.min',
+            'https://ajax.googleapis.com/ajax/libs/mootools/1.6.0/mootools.min.js',
+            'mootools.min'
+        ],
+        swfObj: 'swfobject' 
+    }
+
+    // forget adding swfobject.js it's freaking FLASH!
+});
